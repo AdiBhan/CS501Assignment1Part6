c56bee5380a8f9a52dc63ad96b30551ef87d82c4
AdiBhan <abhan03@bu.edu>
2023-09-23 22:50:30 -0400

    Consolidated logic of clicking on numeric buttons inside one function. Now I can just copy paste event listeners,and pass in the params (for numeric). Next up is to worry about operations

--- app/src/main/java/com/example/assignment2part6/MainActivity.kt
+++ app/src/main/java/com/example/assignment2part6/MainActivity.kt
@@ -14,18 +14,18 @@ class MainActivity : AppCompatActivity() {
 
     // VIEWS
     lateinit var oneButton: Button
-    lateinit var TwoButton: Button
-    lateinit var ThreeButton: Button
-    lateinit var FourButton: Button
-    lateinit var FiveButton: Button
-    lateinit var SixButton: Button
-    lateinit var SevenButton: Button
-    lateinit var EightButton: Button
-    lateinit var NineButton: Button
-    lateinit var EqualButton: Button
-    lateinit var DecimalButton: Button
-    lateinit var ZeroPointZero: TextView
-    lateinit var SqrtButton: Button
+    lateinit var twoButton: Button
+    lateinit var threeButton: Button
+    lateinit var fourButton: Button
+    lateinit var fiveButton: Button
+    lateinit var sixButton: Button
+    lateinit var sevenButton: Button
+    lateinit var eightButton: Button
+    lateinit var nineButton: Button
+    lateinit var equalButton: Button
+    lateinit var decimalButton: Button
+    lateinit var zeroPointZero: TextView
+    lateinit var sqrtButton: Button
     lateinit var clearButton: Button
     lateinit var addButton: Button
     lateinit var subtractButton: Button
@@ -44,43 +44,61 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         oneButton = findViewById(R.id.one_id)
-        TwoButton = findViewById(R.id.two_id)
-        ThreeButton = findViewById(R.id.three_id)
-        FourButton = findViewById(R.id.four_id)
-        FiveButton = findViewById(R.id.five_id)
-        SixButton = findViewById(R.id.six_id)
-        SevenButton = findViewById(R.id.seven_id)
-        EightButton = findViewById(R.id.eight_id)
-        NineButton = findViewById(R.id.nine_id)
-        clearButton = findViewById(R.id.clear)
-        addButton = findViewById(R.id.add)
-        subtractButton = findViewById(R.id.subtract)
-        multiplyButton = findViewById(R.id.multiply)
-        divideButton = findViewById(R.id.divide)
-        EqualButton = findViewById(R.id.equal_id)
-        DecimalButton = findViewById(R.id.decimal_id)
-        ZeroPointZero = findViewById(R.id.zero_point_zero_id)
-        SqrtButton = findViewById(R.id.sqrt_id)
+        twoButton = findViewById(R.id.two_id);
+        threeButton = findViewById(R.id.three_id);
+        fourButton = findViewById(R.id.four_id);
+        fiveButton = findViewById(R.id.five_id);
+        sixButton = findViewById(R.id.six_id);
+        sevenButton = findViewById(R.id.seven_id);
+        eightButton = findViewById(R.id.eight_id);
+        nineButton = findViewById(R.id.nine_id);
+        clearButton = findViewById(R.id.clear);
+        addButton = findViewById(R.id.add);
+        subtractButton = findViewById(R.id.subtract);
+        multiplyButton = findViewById(R.id.multiply);
+        divideButton = findViewById(R.id.divide);
+        equalButton = findViewById(R.id.equal_id);
+        decimalButton = findViewById(R.id.decimal_id);
+        zeroPointZero = findViewById(R.id.zero_point_zero_id);
+        sqrtButton = findViewById(R.id.sqrt_id);
+
 
 
         oneButton.setOnClickListener { view: View ->
+
+            currentAnswer = handleClickNumberButtons(
+                clickedOperation,
+                selectedOperation,
+                currentAnswer,
+                "1",
+                view
+            )
+
+
+        }
+
+
+    }
+
+        private fun handleClickNumberButtons(clickedOperation:Boolean, selectedOperation:String, currentAnswer:String, newNumber:String,view: View): String {
+            var result = currentAnswer
+            // If previous button clicked was an operation
             if (clickedOperation) {
                 // Here, convert currentAnswer to float and then do your operations
                 val currentFloat = currentAnswer.toFloat()
-                currentAnswer =
-                    currentOperation(selectedOperation, currentFloat, 1F, view).toString()
-                ZeroPointZero.text = currentAnswer
+                result = currentOperation(selectedOperation, currentFloat, 1F,view).toString()
+                zeroPointZero.text = result
             } else {
-                if (currentAnswer == "0") {
-                    currentAnswer = "1"
+                if (currentAnswer == newNumber) {
+                    result = newNumber
                 } else {
-                    currentAnswer += "1"
+                    result += newNumber
                 }
-                ZeroPointZero.text = currentAnswer
+                ZeroPointZero.text = result
             }
+            return result
         }
 
-    }
 
     private fun currentOperation(
         operation: String,
