5a9dd42a54ab1ce348231a83dae77d0a478ad315
AdiBhan <abhan03@bu.edu>
2023-09-23 22:29:53 -0400

    Added eventlistener for first button and outlined basic logic for the app.Treating the currentNumber as a string so we can concancate it on when user clicks multiple numbers. Then added a currentOperation function which takes in the currentOperation and adds the last two numbers entered. Also added error handling checks. Next step is to add event listeners for rest of methods

--- app/src/main/java/com/example/assignment2part6/MainActivity.kt
+++ app/src/main/java/com/example/assignment2part6/MainActivity.kt
@@ -2,36 +2,47 @@ package com.example.assignment2part6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import com.example.assignment2part6.R.id.one_id
+import com.google.android.material.snackbar.Snackbar
+import kotlin.math.sqrt
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var OneButton:Button
-    lateinit var TwoButton:Button
-    lateinit var ThreeButton:Button
-    lateinit var FourButton:Button
-    lateinit var FiveButton:Button
-    lateinit var SixButton:Button
-    lateinit var SevenButton:Button
-    lateinit var EightButton:Button
-    lateinit var NineButton:Button
-    lateinit var AddButton:Button
-    lateinit var SubtractButton:Button
-    lateinit var MultiplyButton:Button
-    lateinit var DivideButton:Button
-    lateinit var EqualButton:Button
-    lateinit var DecimalButton:Button
-    lateinit var ZeroPointZero:Button
-    lateinit var SqrtButton:Button
-    lateinit var DisplayText: EditText
+
+    // VIEWS
+    lateinit var oneButton: Button
+    lateinit var TwoButton: Button
+    lateinit var ThreeButton: Button
+    lateinit var FourButton: Button
+    lateinit var FiveButton: Button
+    lateinit var SixButton: Button
+    lateinit var SevenButton: Button
+    lateinit var EightButton: Button
+    lateinit var NineButton: Button
+    lateinit var AddButton: Button
+    lateinit var SubtractButton: Button
+    lateinit var MultiplyButton: Button
+    lateinit var DivideButton: Button
+    lateinit var EqualButton: Button
+    lateinit var DecimalButton: Button
+    lateinit var ZeroPointZero: TextView
+    lateinit var SqrtButton: Button
+
+
+    // STATE VARIABLES FOR OPERATION
+    var currentAnswer: String = "0"
+    var clickedOperation: Boolean = false
+    var selectedOperation: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        OneButton = findViewById(one_id)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        oneButton = findViewById(R.id.one_id)
         TwoButton = findViewById(R.id.two_id)
         ThreeButton = findViewById(R.id.three_id)
         FourButton = findViewById(R.id.four_id)
@@ -48,11 +59,72 @@ class MainActivity : AppCompatActivity() {
         DecimalButton = findViewById(R.id.decimal_id)
         ZeroPointZero = findViewById(R.id.zero_point_zero_id)
         SqrtButton = findViewById(R.id.sqrt_id)
-        DisplayText = findViewById(R.id.display_text_id)
 
 
+        oneButton.setOnClickListener { view: View ->
+            if (clickedOperation) {
+                // Here, convert currentAnswer to float and then do your operations
+                val currentFloat = currentAnswer.toFloat()
+                currentAnswer =
+                    currentOperation(selectedOperation, currentFloat, 1F, view).toString()
+                ZeroPointZero.text = currentAnswer
+            } else {
+                if (currentAnswer == "0") {
+                    currentAnswer = "1"
+                } else {
+                    currentAnswer += "1"
+                }
+                ZeroPointZero.text = currentAnswer
+            }
+        }
 
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
+
+    private fun currentOperation(
+        operation: String,
+        firstNum: Float,
+        secondNum: Float,
+        view: View
+    ): Float {
+
+        if (operation == "add") {
+            return firstNum + secondNum
+        }
+        if (operation == "subtract") {
+            return firstNum - secondNum
+        }
+        if (operation == "multiply") {
+            return firstNum * secondNum
+        }
+        if (operation == "divide") {
+            // Handling error checking where user tries to divide by zero
+            if (secondNum == 0.0F) {
+                Snackbar.make(
+                    view,
+                    "Error,you cannot divide by zero. Please try with a different number",
+                    Snackbar.LENGTH_LONG
+                ).show()
+                return 0.0F
+            } else {
+                return firstNum / secondNum
+            }
+        }
+        if (operation == "SQRT") {
+            // Handling error checking where user tries to SQRT negative number
+            if (firstNum < 0) {
+                Snackbar.make(
+                    view,
+                    "Error, you cannot SQRT a number less than or equal to zero. Please restart and try again with a positive number!",
+                    Snackbar.LENGTH_LONG
+                ).show()
+                return 0.0F
+            } else {
+                return sqrt(firstNum)
+            }
+        }
+        return 0.0F
     }
 }
+
+
+
